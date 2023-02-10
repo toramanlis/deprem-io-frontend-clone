281d3be472744287094a70da37f98892b657428d
fatih ozdil <muhammedfatihozdil@gmail.com>
2023-02-10 15:17:09 +0300

    detay sayfasinda arac durumu backend baglantisi eklendi

--- assets/js/requests/detail.js
+++ assets/js/requests/detail.js
@@ -66,9 +66,22 @@ function getItem() {
     item = item.results;
     const acilDurum = item.acilDurum;
     const yardimDurum = item.yardimDurumu;
-
+    const aracDurum = item.fields.aracDurumu;
+    console.log(aracDurum);
     status.getElementsByTagName("p")[0].innerHTML =
       yardimDurum + " - " + acilDurum;
+
+    if (aracDurum === "var") {
+      document.getElementById("araciVar").checked = true;
+      document.getElementById("araciYok").disabled = true;
+    } else if (aracDurum === "yok") {
+      document.getElementById("araciVar").disabled = true;
+      document.getElementById("araciYok").checked = true;
+    } else {
+      document.getElementById("araciVar").disabled = true;
+      document.getElementById("araciYok").disabled = true;
+    }
+
     if (acilDurum === "kritik") {
       document.getElementById("kritik").checked = true;
       document.getElementById("orta").disabled = true;
